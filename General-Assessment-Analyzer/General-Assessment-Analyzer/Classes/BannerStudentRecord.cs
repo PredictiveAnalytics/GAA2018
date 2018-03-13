8a33285d47da19cd5db919366bd312a13e7703fd
masterofzion <masterofzion@gmail.com>
2018-03-13 11:23:23 -0500

    Revision Commit

---
+++ General-Assessment-Analyzer/General-Assessment-Analyzer/Classes/BannerStudentRecord.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace General_Assessment_Analyzer.Classes
+{
+    [Serializable]
+    public class BannerStudentRecord
+    {
+        [XmlElement("ID")]
+        public string ID { get; set; }
+        [XmlElement("Last")]
+        public string Last { get; set; }
+        [XmlElement("First")]
+        public string First { get; set; }
+        [XmlElement("Middle")]
+        public string Middle { get; set; }
+        [XmlElement("Degree")]
+        public string Degree { get; set; }
+        [XmlElement("MajorCode")]
+        public string MajorCode { get; set; }
+        [XmlElement("MajorDesc")]
+        public string MajorDesc { get; set; }
+        [XmlElement("RateCode")]
+        public string RateCode { get; set; }
+        [XmlElement("RateDesc")]
+        public string RateDesc { get; set; }
+        [XmlElement("User")]
+        public string User { get; set; }
+        [XmlElement("Cohort")]
+        public string Cohort { get; set; }
+
+        public BannerStudentRecord()
+        {
+
+        }
+
+    }
+}
---
+++ General-Assessment-Analyzer/General-Assessment-Analyzer/Classes/ScalePoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace General_Assessment_Analyzer.Classes
+{
+    [Serializable]
+    public class ScalePoint
+    {
+        [XmlElement("Label")]
+        public string Label { get; set; }
+        [XmlElement("Value")]
+        public int Value { get; set; }
+
+        public int CompareTo(ScalePoint other)
+        {
+            return Value.CompareTo(other.Value);
+        }
+
+        public ScalePoint()
+        {
+
+        }
+
+        public ScalePoint(string lb, int val)
+        {
+            Label = lb;
+            Value = val;
+        }
+    }
+}
